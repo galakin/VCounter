b45224a46723f14e905df205f26c81aa371cdda4
galakin <jacopo.pela@liber.it>
2020-07-25 15:46:49 +0200

    safety commit

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,1 +1,18 @@
 import 'package:sqflite/sqflite.dart';
+
+class LocalDatabase {
+  Database db;
+
+  Future openDB(){
+    var databasesPath = await getDatabasesPath();
+    String path = join(databasesPath, 'localdb.db');
+    var db = await openDatabase(
+      path,
+      version: 1,
+      onCreate: (Database db, int version) async {
+        await db.execute('CREATE TABLE Games (id INTEGER PRIMARY KEY, player1 TEXT, player2 TEXT, life1 INTEGER, life2 INTEGER)');
+      }
+    );
+
+  }
+}
