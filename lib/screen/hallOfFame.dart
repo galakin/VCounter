bcfb30fabf08244eea269ed353fa08b6ad9a2e17
galakin <jacopopela@libero.it>
2020-12-27 13:09:21 +0100

    add scaffold template

--- lib/screen/hallOfFame.dart
+++ lib/screen/hallOfFame.dart
@@ -3,6 +3,7 @@ import 'package:flutter_redux/flutter_redux.dart';
 import 'package:redux/redux.dart';
 
 import 'package:vcounter/resources/drawer.dart';
+import 'package:vcounter/resources/scaffold.dart';
 
 class HallOfFame extends StatelessWidget{
   Store _store;
@@ -10,10 +11,14 @@ class HallOfFame extends StatelessWidget{
   HallOfFame(this._store);
 
   @override Widget build(BuildContext context){
-    return Scaffold(
-      drawer: VDrawer(_store),
-      body: Center(child: Text('Questa pagina è più vuota\n del tuo bicchiere!', textAlign: TextAlign.center, style: TextStyle(fontSize: 28.0, fontWeight: FontWeight.bold)))
-      /*TODO center correctly the text*/
-    );//end Scaffold
+    return MainScaffold(
+      _store,
+      Center(child: Text('Questa pagina è più vuota\n del tuo bicchiere!', textAlign: TextAlign.center, style: TextStyle(fontSize: 28.0, fontWeight: FontWeight.bold))),
+    );
+    // return Scaffold(
+    //   drawer: VDrawer(_store),
+    //   body:
+    //   /*TODO center correctly the text*/
+    // );//end Scaffold
   }
 }
