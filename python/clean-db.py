a048898c54882ba8380fa7bd504ca0cd7483c111
galakin <jacopo.pela@libero.it>
2021-08-23 14:15:56 +0200

    safety push

--- python/clean-db.py
+++ python/clean-db.py
@@ -4,19 +4,23 @@ from firebase_admin import firestore, credentials, db
 import os
 if __name__ == "__main__":
     credential = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
-    #print(credential)
     cred = credentials.Certificate(credential)
     firebase.initialize_app(cred, {'projectId': 'vcounter-6d83c'})               #initialize firebase app
     
-    #Clinet = firestore.client()
     db = firestore.client()
     db_ref = db.collection(u'saved-games')
     #print("Hello There!")
-    print(db_ref)
     docs = db_ref.stream()
-    
-    for elem in docs:
-        print(f'{elem.id} => {elem.to_dict()}')
-        #print(elem)
-        None
+    del_elem = []    
 
+
+    counter = 0
+    for elem in docs:
+        elem_dict = elem.to_dict()
+        if elem_dict['date'] == 'Null':
+            #print(elem_dict)
+            del_elem.append(elem_dict)
+    
+        #print(f'{elem.id} => {elem.to_dict()}')
+        counter += 1
+    print("you have "+str(len(del_elem))+" elem to delete!")
